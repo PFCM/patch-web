30cdb00260accd2dbf53161fdd719f51f7ecdd1c
pfcm <pfcmathews@gmail.com>
2019-04-08 21:20:31 +1000

    try to log in as a service account

--- patchserver/cf-deploy.sh
+++ patchserver/cf-deploy.sh
@@ -7,13 +7,14 @@ PROJECT_NAME=${GCP_PROJECT_ID}
 ENTRY_POINT="cf_process"
 RUNTIME="python37"
 REGION=${GCP_REGION:-us-central-1}
-SOURCE_BUCKET=${GCP_SOURCE_BUCKET:-patchies-source}
+SOURCE_BUCKET=${GCP_SOURCE_BUCKET}
 
 echo "===packaging==="
 docker build -t cf-build -f Dockerfile.build .
 docker create -ti --name build cf-build bash
 docker cp build:/tmp/package.zip ./package.zip
-file package.zip
+
+gsutil cp package.zip gs://${SOURCE_BUCKET}/
 
 gcloud functions deploy ${FUNCTION_NAME} \
   --entry-point ${ENTRY_POINT} \
